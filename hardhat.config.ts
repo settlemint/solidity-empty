8786833b9616d098327e5cb758f40ae5db1b374e
Roderik van der Veer <roderik@settlemint.com>
2024-05-23 16:11:21 +0200

    wip

---
+++ hardhat.config.ts
@@ -0,0 +1,9 @@
+import "@nomicfoundation/hardhat-foundry";
+import "@nomicfoundation/hardhat-toolbox-viem";
+import type { HardhatUserConfig } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.26",
+};
+
+export default config;
